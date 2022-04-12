c62783c5c200181d8a4398b998168a5aaf6944dd
Jamil Lawrence <jamil.lawrence@gmail.com>
2022-04-12 10:02:57 -0500

    Adding in React Router

--- FrontEnd/src/components/Header.jsx
+++ FrontEnd/src/components/Header.jsx
@@ -1,8 +1,21 @@
 
+import { Outlet, Link } from "react-router-dom";
+
 const Header = () => {
   return (
-    <div className="border border-b-2 text-4xl text-slate-100 bg-slate-800 flex justify-center">
-      <span className="">Header Goes Here</span>
+    <div className="border border-b-2 text-4xl text-slate-100 bg-slate-800 grid justify-items-center">
+      <h2 className="">Where Does It Go?? (Your time)</h2>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+        className="text-orange-200 "
+      >
+        <Link to="/allotments">Allotments</Link> |{" "}
+        <Link to="/charts">Charters</Link>
+      </nav>
+      <Outlet />
     </div>
   );
 };
--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+        </Route>
+        <Route
+            path="*"
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>There's nothing here!</p>
+              </main>
+            }
+          />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
